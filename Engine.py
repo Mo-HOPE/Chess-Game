8625579335e4abcfa33460a537967d6fc190f124
Mohamed AbdulRaouf <120792018+Mo-HOPE@users.noreply.github.com>
2023-07-02 21:25:52 +0300

    create all pawns moves
    create all valid possible moves for pawns inside "pawn_move" method
    
    return True if it is valid
    return False if it isn't

--- Engine.py
+++ Engine.py
@@ -51,11 +51,49 @@ class Engine:
     def pawn_move(self):
 
         # white pawn valid move
-        if Engine.board[self.row][self.col][0] == "w":
-            if Engine.board[self.new_row][self.new_col] != "w":
-                if self.new_col != self.col:
-                    return False
+        if Engine.board[self.row][self.col][0] == "w":           # check if the piece is a white pawn
+            if Engine.board[self.new_row][self.new_col] != "w":  # check if the new square doesn't have a friendly piece
+                if (Engine.board[self.new_row][self.new_col] == "*") and (self.col == self.new_col):  # empty square
 
-                if self.new_row == self.row-1:
-                    return True
+                    if self.row == 6:                            # if it's the first move, pawn can move twice or once
+                        if (self.new_row == self.row-1) or (self.new_row == self.row-2):
+                            return True
+                        else:
+                            return False
+                    else:                                        # if it's not the first move, pawn can move just one
+                        if self.new_row == self.row-1:
+                            return True
+                        else:
+                            return False
 
+                if Engine.board[self.new_row][self.new_col][0] == "b":                   # attacking case
+                    if (self.new_row == self.row-1) and (self.new_col == self.col+1):    # take piece on the right
+                        return True
+                    elif (self.new_row == self.row-1) and (self.new_col == self.col-1):  # take piece on the left
+                        return True
+                    else:
+                        return False
+
+        # black pawn valid moves
+        if Engine.board[self.row][self.col][0] == "b":           # check if the piece is a black pawn
+            if Engine.board[self.new_row][self.new_col] != "b":  # check if the new square doesn't have a friendly piece
+                if (Engine.board[self.new_row][self.new_col] == "*") and (self.col == self.new_col):  # empty square
+
+                    if self.row == 1:                            # if it's the first move, pawn can move twice or once
+                        if (self.new_row == self.row+1) or (self.new_row == self.row+2):
+                            return True
+                        else:
+                            return False
+                    else:                                        # if it's not the first move, pawn can move just one
+                        if self.new_row == self.row+1:
+                            return True
+                        else:
+                            return False
+
+                if Engine.board[self.new_row][self.new_col][0] == "w":                   # attacking case
+                    if (self.new_row == self.row+1) and (self.new_col == self.col+1):    # take piece on the right
+                        return True
+                    elif (self.new_row == self.row+1) and (self.new_col == self.col-1):  # take piece on the left
+                        return True
+                    else:
+                        return False
